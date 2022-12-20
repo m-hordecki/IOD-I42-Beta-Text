bfad667d32c3649ce9cd8ca02eebeac378f54b59
Pojemnik <andriej194@gmail.com>
2022-12-20 18:49:56 +0100

    Add javadoc plugin

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -3,10 +3,17 @@ package pl.put.poznan.transformer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * TextTransformerApplication class javadoc test
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
+    /**
+     * Main method.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
     }
