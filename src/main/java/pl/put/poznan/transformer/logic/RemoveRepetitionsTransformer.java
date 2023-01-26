11ff2454b2e8db4f391bc3fb7ba78ab45c67c94e
m-hordecki <65454634+m-hordecki@users.noreply.github.com>
2023-01-26 14:20:12 +0100

    Added JavaDoc

--- src/main/java/pl/put/poznan/transformer/logic/RemoveRepetitionsTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/RemoveRepetitionsTransformer.java
@@ -6,15 +6,24 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+/**
+ * Removes repetitions in given input
+ */
 public class RemoveRepetitionsTransformer extends TextTransformerDecorator
 {
     private static final Logger logger = LoggerFactory.getLogger(RemoveRepetitionsTransformer.class);
-
+    /**
+     * Creates TextTransformer input for further repetitions removal
+     */
     public RemoveRepetitionsTransformer(TextTransformer component) {
         super(component);
     }
-
+    /**
+     * Removes repetitions
+     *
+     * @param text string input given to remove repetitions
+     * @return string removed repetitions
+     */
     @Override
     public String transform(String text) {
         String transformed = super.transform(text);
