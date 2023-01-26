47dcde10f1fce1161651da44f1556bdf679ac617
m-hordecki <65454634+m-hordecki@users.noreply.github.com>
2023-01-26 14:17:34 +0100

    Added JavaDoc

--- src/main/java/pl/put/poznan/transformer/logic/AbbreviationRetractTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/AbbreviationRetractTransformer.java
@@ -3,15 +3,24 @@ package pl.put.poznan.transformer.logic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.put.poznan.transformer.data.Abbreviations;
-
+/**
+ * Retracts abbrevations in given input
+ */
 public class AbbreviationRetractTransformer extends TextTransformerDecorator
 {
     private static final Logger logger = LoggerFactory.getLogger(AbbreviationRetractTransformer.class);
-
+    /**
+     * Creates TextTransformer input for further abbrevations retraction
+     */
     public AbbreviationRetractTransformer(TextTransformer component) {
         super(component);
     }
-
+    /**
+     * Retracts abbrevations
+     *
+     * @param text string input given to retract abbrevations
+     * @return string with retracted abbreaviations 
+     */
     @Override
     public String transform(String text) {
         String transformed = super.transform(text);
