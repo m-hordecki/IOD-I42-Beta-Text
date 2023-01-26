95e03739ef655f0c0f089d7343797f23b3fd49a2
m-hordecki <65454634+m-hordecki@users.noreply.github.com>
2023-01-26 14:08:55 +0100

    Added Java Doc

--- src/main/java/pl/put/poznan/transformer/logic/InvertWordsTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/InvertWordsTransformer.java
@@ -3,12 +3,25 @@ package pl.put.poznan.transformer.logic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Inverts words in given input
+ */
+
 public class InvertWordsTransformer extends TextTransformerDecorator {
     private static final Logger logger = LoggerFactory.getLogger(ToLowercaseTransformer.class);
-
+    /**
+     * Creates TextTransformer input for further capitalization
+     */
     public InvertWordsTransformer(TextTransformer component) {
         super(component);
     }
+    
+    /**
+     * Inverts every word
+     *
+     * @param text string input given to invert
+     * @return inverted string
+     */
 
     @Override
     public String transform(String text) {
